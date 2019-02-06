2ba160bb8af2a51100b558beeb68bcfcff22dc37
Sachin <sachinjois@gmail.com>
2019-02-06 08:54:05 +0530

    2. Builder pattern demo

--- src/main/java/com/java/example/LombokBasic.java
+++ src/main/java/com/java/example/LombokBasic.java
@@ -1,7 +1,11 @@
 package com.java.example;
 
+import com.java.example.model.Person;
 import com.java.example.model.User;
 
+import lombok.extern.java.Log;
+
+@Log
 public class LombokBasic {
 
 	public static void main(String[] args) {
@@ -11,9 +15,18 @@ public class LombokBasic {
 		user.setAge(20);
 		user.setFirstName("Sachin");
 		user.setLastName("NS");
-		System.out.println(user.toString());
+		log.info(user.toString());
 		StringUtils.line();
-		
+
+		// 2. Builder class annotation
+		Person person = Person.builder().age(10)
+							.gender('F').name("Monali")
+							.build();
+		log.info(person.toBuilder().toString());
+		log.info("Person Name is " + person.getName());
+		log.info("Age is "+ person.getAge());
+		log.info("Phone No. is "+ person.getPhoneNumber());
+		StringUtils.line();
 	}
 
 }
