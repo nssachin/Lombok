eb49325183d5cbb6a2729566ce748825894a089e
Sachin <sachinjois@gmail.com>
2019-02-06 08:37:58 +0530

    1. @Data - Pojo Setters, Getters, toString demo

--- src/main/java/com/java/example/LombokBasic.java
+++ src/main/java/com/java/example/LombokBasic.java
@@ -6,12 +6,20 @@ public class LombokBasic {
 
 	public static void main(String[] args) {
 
+		// 1. Pojo getter, Setter
 		User user = new User();
 		user.setAge(20);
-		user.setFirstName("Name");
-		user.setLastName("last");
-
+		user.setFirstName("Sachin");
+		user.setLastName("NS");
 		System.out.println(user.toString());
+		StringUtils.line();
+		
 	}
 
+}
+
+abstract class StringUtils {
+	static void line() {
+		System.out.println("=================================================");
+	}
 }
